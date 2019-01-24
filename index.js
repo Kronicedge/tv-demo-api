c30a0457419563accef3adebf73cff6c7e1b5d77
Myles Timothy <myles_timothy@Outlook.com>
2019-01-23 19:43:41 -0700

    Setup GET,POST,PUT,DELETE

--- index.js
+++ index.js
@@ -2,12 +2,36 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
+const tvShows = []
 
+app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+   })
 
-app.get('/shows', (req, res) => res.send('Hello World!'))
-app.post('/shows', (req, res) => res.send('Hello World!'))
-app.putt('/shows', (req, res) => res.send('Hello World!'))
+app.get('/shows/', (req, res) => res.send(tvShows))
+
+app.post('/shows', (req, res) => {
+    const tvShow = {
+        name: req.body.show,
+        rating: req.body.rating,
+        img: req.body.imgURL
+    }
+    tvShows.push(tvShow)
+
+    console.log(tvShow)
+
+    res.send(tvShow)
+})
+
+app.put('/shows', (req, res) => res.send('Hello World!'))
+
 app.delete('/shows', (req, res) => res.send('Hello World!'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
