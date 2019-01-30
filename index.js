2f3a1910c1021930482b0925bd01ba9c4c96c5e8
Myles Timothy <myles_timothy@Outlook.com>
2019-01-29 19:44:25 -0700

    MONGO: TV Demo (Part 14)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('monk')('mongodb://admin:password1@ds054289.mlab.com:54289/tv-demo-db')
+const tvShowCollection = db.get('tvShow')
 const app = express()
 const port = 4000
 const tvShows = []
@@ -7,23 +9,26 @@ const tvShows = []
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
    })
 
-app.get('/shows/', (req, res) => res.send(tvShows))
+app.get('/shows/', async (req, res) => {
+    const tvShow = await tvShowCollection.find(req.params.id)
+    res.send(tvShow)
+})
 
-app.post('/shows', (req, res) => {
-    const tvShow = {
-        name: req.body.show,
-        rating: req.body.rating,
-        img: req.body.imgURL
-    }
-    tvShows.push(tvShow)
+app.get('/shows/', async (req, res) => {
+    const tvShows = await tvShowCollection.find({})
+    res.send(tvShows)
+})
 
+app.post('/shows', async (req, res) => {
+    const tvShow = await tvShowCollection.insert(req.body)
+    
     console.log(tvShow)
 
     res.send(tvShow)
